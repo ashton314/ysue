7e4198e6163056bcbd207b27d6a1649afb517aff
Ashton Wiersdorf <mail@wiersdorf.dev>
2024-03-29 17:27:13 -0600

    Create placeholder module for terminal manipulation

---
+++ src/Display.hs
@@ -0,0 +1,7 @@
+module Display () where
+
+
+
+-- See
+-- https://hackage.haskell.org/package/vty-6.2/docs/Graphics-Vty.html
+-- https://hackage.haskell.org/package/vty-6.2/docs/Graphics-Vty-Image.html#t:Image
